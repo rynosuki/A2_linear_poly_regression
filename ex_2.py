4fbdba7b9b1bbaeba29654b866fa33f9d0b9513b
Robin Svensson <rs223dj@student.lnu.se>
2022-04-11 22:28:23 +0200

    first commit

---
+++ ex_2.py
@@ -0,0 +1,95 @@
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+from numba import njit
+
+@njit
+def normalized(Xi):
+  mu = np.mean(Xi)
+  omega = np.std(Xi)
+  return (Xi - mu) / omega
+
+@njit
+def normal_eq(Xe, y):
+  return inv(np.dot(Xe.T,Xe)).dot(Xe.T).dot(y)
+
+@njit
+def cost_eq(Xe, beta, y):
+  return ((np.dot(Xe,beta.astype(Xe.dtype))- y).T.dot(np.dot(Xe,beta.astype(Xe.dtype)) - y))/len(Xe)
+
+def gradient_eq(Xe, beta, y, alpha):
+  return beta - np.dot(alpha,Xe.T).dot(np.dot(Xe,beta) - y)
+
+@njit
+def mse(pred_y, y):
+  return ((pred_y - y)**2).mean()
+
+X = np.genfromtxt("housing_price_index.csv", dtype = np.float32, delimiter=",")
+
+y = X[:,1]
+X = X[:,0]
+plt.plot(X,y,"ro")
+plt.show()
+
+polys = [np.c_[np.ones((len(X), 1)), X],np.c_[np.ones((len(X), 1)), X**1, X**2], 
+          np.c_[np.ones((len(X), 1)), X**1, X**2, X**3], np.c_[np.ones((len(X), 1)), X**1, X**2, X**3, X**4]]
+values = []
+mses = []
+
+for Xe in polys:
+  beta = normal_eq(Xe.astype(np.float32), y)
+  values.append(np.dot(Xe.astype(np.float32),beta))
+  
+for i in range(len(values)):
+  plt.subplot(2,2,i+1)
+  plt.scatter(X, y, edgecolors="red", c="none")
+  plt.plot(X, values[i])
+plt.show()
+
+for i in values:
+  mses.append(mse(i, y))
+# Polynomial 4 chosen due to the lowest MSE
+# Values calculated were:
+# 2896.09375
+# 585.0675048828125
+# 454.5433349609375
+# 444.0847473144531
+# Respectively per polynomial.
+
+# Ex _ 3
+beta = normal_eq(polys[3].astype(np.float32), y)
+print(np.dot([1, 47, 47**2, 47**3, 47**4],beta))
+# He bought at index 568 and would now be worth 797
+# print(2.3e+6*(797/568))
+# The house would be worth 3.227 million SEK in 2022
+
+
+#### Calculations with gradient.
+values = []
+Xn = normalized(X)
+print("Normalized mean:", np.mean(Xn), "Non-normalized: ",np.mean(X))
+print("Normalized STD:", np.std(Xn), "Non-normalized:", np.std(X))
+polys = [np.c_[np.ones((len(Xn), 1)), Xn],np.c_[np.ones((len(Xn), 1)), Xn**1, Xn**2], 
+          np.c_[np.ones((len(Xn), 1)), Xn**1, Xn**2, Xn**3], np.c_[np.ones((len(Xn), 1)), Xn**1, Xn**2, Xn**3, Xn**4]]
+x3 = np.c_[np.ones((len(X), 1)), X**1, X**2, X**3, X**4]
+beta = np.zeros(polys[0].shape[1])
+for n in range(50):
+  beta = gradient_eq(polys[0], beta, y, 0.003)
+  values.append(cost_eq(polys[0], beta, y))
+beta = np.zeros(polys[1].shape[1])
+for n in range(50):
+  beta = gradient_eq(polys[1], beta, y, 0.015)
+  values.append(cost_eq(polys[1], beta, y))
+beta = np.zeros(polys[2].shape[1])
+for n in range(250):
+  beta = gradient_eq(polys[2], beta, y, 0.0095)
+  values.append(cost_eq(polys[2], beta, y))
+beta = np.zeros(polys[3].shape[1])
+for n in range(250):
+  beta = gradient_eq(polys[3], beta, y, 0.0041)
+  values.append(cost_eq(polys[3], beta, y))
+  
+print("For d:1 alpha =", 0.003, "with n of", 50)
+print("For d:2 alpha =", 0.0015, "with n of", 50)
+print("For d:3 alpha =", 0.0095, "with n of", 250)
+print("For d:4 alpha =", 0.0041, "with n of", 250)
---
+++ ex_a.py
@@ -0,0 +1,57 @@
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
+def normalized(Xi):
+  mu = np.mean(Xi)
+  omega = np.std(Xi)
+  return (Xi - mu) / omega
+
+def normal_eq(Xe, y):
+  return inv(np.dot(Xe.T,Xe)).dot(Xe.T).dot(y)
+
+def cost_eq(Xe, beta, y):
+  return ((np.dot(Xe,beta)- y).T.dot(np.dot(Xe,beta) - y))/len(Xe)
+
+def gradient_eq(Xe, beta, y, alpha):
+  return beta - np.dot(alpha,Xe.T).dot(np.dot(Xe,beta) - y)
+
+X = np.genfromtxt("girls_height.csv", dtype = np.float64, delimiter="	")
+
+#Ex _ 1
+fig, (ax1, ax2) = plt.subplots(1,2)
+ax1.plot(X[:,2], X[:,0], "go")
+ax2.plot(X[:,1], X[:,0], "go")
+plt.show()
+
+#Ex _ 2
+Xe = np.c_[np.ones((len(X[:,0]),1)), X[:,1], X[:,2]]
+y = X[:,0]
+print(y.shape)
+
+#Ex _ 3
+beta = normal_eq(Xe, y)
+model = np.dot(Xe, beta)
+# print(model[2])
+
+#Ex _ 4
+x1 = normalized(X[:,1])
+x2 = normalized(X[:,2])
+Xe = np.c_[np.ones((len(X[:,0]))), x1, x2]
+
+#Ex _ 5
+beta = normal_eq(Xe, y)
+model = Xe.dot(beta)
+
+#Ex _ 6
+# print(cost_eq(Xe, beta, y))
+
+#Ex _ 7
+beta = [18.50, 0.303, 0.388]
+for n in range(30):
+  beta = gradient_eq(Xe, beta, y, 0.002)
+  plt.plot(n, cost_eq(Xe, beta, y), "ro")
+print(cost_eq(Xe, beta, y))
+plt.show()
+print("Suitable alpha and N would be alpha = 0.002 and N = 30")
+print(Xe.dot(beta)[2])
